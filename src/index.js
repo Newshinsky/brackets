bc1e435940e94873e8898f914c52ea1e45a97d93
Newshinsky <72308491+Newshinsky@users.noreply.github.com>
2021-03-01 20:13:01 +0300

    feat: implement brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newArr = [];
+for (let i of bracketsConfig) {
+i = i.toString().replace("," , "");
+newArr.push(i);
+}
+console.log(newArr)
+for (i = 0; i<str.length; i++){
+	if (str.includes(newArr[i])) {
+  str = str.replace(newArr[i], '');
+  i = -1;
+  }
+  if (str.length == 0){
+  return true;
+  }
+}
+   return  false;
 }
